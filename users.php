21b47b1db105b45163cef0bc76a1f7b1b4650ce6
Bekir-B <face995@gmail.com>
2016-09-15 16:14:52 +0200

    Fourth commit (users page now shows email column and added the login page)

--- users.php
+++ users.php
@@ -10,7 +10,7 @@
         if (mysqli_num_rows($result) > 0) {
             while ($row = mysqli_fetch_assoc($result)) {
 
-                echo "ID: " .$row["UserID"]. " Name: " .$row["Name"]. " " .$row["Surname"]. " Date Joined: " .$row["DateJoined"]. "<br>" ;
+                echo "ID: " .$row["UserID"]. " Name: " .$row["Name"]. " " .$row["Surname"]. " Date Joined: " .$row["DateJoined"]. " E-mail: " .$row["Email"]. "<br>" ;
             }
         }
                 else {
