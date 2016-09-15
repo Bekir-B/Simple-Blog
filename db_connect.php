390899b852c1dbac8ba9e66258ad722f6c07ca3a
Bekir-B <face995@gmail.com>
2016-09-15 16:14:52 +0200

    Fourth commit (users page now shows email column and added the login page)

--- db_connect.php
+++ db_connect.php
@@ -5,4 +5,4 @@ DEFINED('DB_HOST') || DEFINE ('DB_HOST', 'localhost');
 DEFINED('DB_NAME') || DEFINE ('DB_NAME', 'testdatabase');
 
 // Make the connection:
-$conn = @mysqli_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR die ('Could not connect to MySQL: ' . mysqli_connect_error() );
+$conn = mysqli_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR die ('Could not connect to MySQL: ' . mysqli_connect_error() );
---
+++ login.php
@@ -0,0 +1,41 @@
+<?php 
+session_start();
+if($_SERVER["REQUEST_METHOD"] == "POST")  {
+   if (empty($_POST['username']) || empty($_POST['password'])) {
+$error = "Username or Password is invalid";
+}
+else
+{
+    $username=$_POST['username'];
+    $password=$_POST['password'];
+
+include ('db_connect.php');
+
+$sql = "SELECT * FROM users WHERE Password='$password' AND Username='$username'";
+$result = mysqli_query($conn,$sql);
+$count = mysqli_num_rows($result);
+
+if($count == 1){
+    $_SESSION['login_user'] = $username;
+    header('Location: users.php');
+}
+else {
+    $error = "Username or Password is invalid";
+}
+ 
+    
+require ('end_connection.php');
+}}
+ ?>
+
+<form action="" method="post">
+
+    Username:<br>
+    <input type="text" name="username"><br>
+
+    Password:<br>
+    <input type="text" name="password"><br>
+
+    <input type="submit" name="submit" value="Login">
+
+</form>
