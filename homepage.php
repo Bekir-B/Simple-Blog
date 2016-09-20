3cbeed7a357b20a392585ffb50c9ef166d3446e4
Bekir-B <face995@gmail.com>
2016-09-20 11:51:33 +0200

    Styled homepage, create a post working

--- homepage.php
+++ homepage.php
@@ -8,8 +8,35 @@
 
 <!--###########################################################-->
     
-<div class="container">
-    <div id="post">
-        
-    </div>
+<div class="wrapper col1">
+
+        <?php
+
+            $query = "SELECT * FROM posts ORDER BY DatePosted DESC LIMIT 5";
+            $result = mysqli_query($conn,$query);
+            $count = mysqli_num_rows($result);
+
+            if($count > 0){
+
+                while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+
+                    $news_body = strip_tags($row['Content']);
+                    if (strlen($news_body) > 2000) {
+                    $stringCut = substr($news_body, 0, 170);
+                    // make sure it ends in a word so assassinate doesn't become ass...
+                    $news_body = substr($stringCut, 0, strrpos($stringCut, ' ')). '...';
+                }
+                    echo '<div class="post">';
+                    echo '<div id=title>';
+                    echo '<h2>' . $row['Title'] . '</h2>';
+                    echo '</div>';
+                    echo '<div id=body>';
+                    echo $news_body;
+                    echo '</div>';
+                    echo '<a href="#" class="button">Continue Reading &rarr;</a>';
+                    echo '</div>';
+            }
+            }
+        ?>
+
 </div>
