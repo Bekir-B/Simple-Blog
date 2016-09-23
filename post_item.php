44665fc8a3aa372f04a83299b6a123b2e8e35b97
Bekir-B <face995@gmail.com>
2016-09-23 11:19:32 +0200

    Comments working and to write a comment, user must be logged in

--- post_item.php
+++ post_item.php
@@ -17,12 +17,54 @@
     echo'</div>';
 ?>
 
+<?php
+
+    $query4 = "SELECT * FROM comments WHERE PostID ={$_REQUEST['PostID']}";
+    $result4 = mysqli_query ($conn, $query4);
+    echo '<div id="comments">';
+    echo '<h2>Comments</h2>';
+    echo '<ul class="commentlist">';
+    echo'</div>';
+?>
+
+
+<?php 
+    if (isset($_SESSION["login_user"])){
+?>
+<div id="postwrap">
 <div id="respond">
     <form action="" method="post">
         <textarea name="content" cols="100%" rows="10"></textarea><br>
-         <input type="hidden" name="userID" value="" />
+         <input type="hidden" name="UserID" value="<?php $result2?>" />
+         <input type="hidden" name="PostID" value="<?php echo $_REQUEST['PostID']; ?>" />
         <input type="submit" name="submit" value="Submit comment" />
-
     </form>
     </div>
+    </div>
 </div>
+
+<?php
+      if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+                $errors = array();
+
+           if (empty($_POST['content'])) {
+                $errors[] = 'You forgot to enter a a comment.';
+            } else {
+                $content = mysqli_real_escape_string($conn, trim($_POST['content']));
+            }
+
+            if (empty($errors)) {
+                
+                $query2="SELECT UserID FROM users WHERE Username = '{$_SESSION["login_user"]}'";
+                $result2=mysqli_query($conn,$query2);
+                $row = mysqli_fetch_assoc($result2);
+                $UID = implode ("",$row);
+                $PID = $_POST['PostID'];
+
+                $query3 = "INSERT INTO comments (Content, PostID, UserID) VALUES ('$content', '$PID', '$UID')"; 
+                $result3 = mysqli_query($conn, $query3);
+                
+            }
+      }  }
+?>
